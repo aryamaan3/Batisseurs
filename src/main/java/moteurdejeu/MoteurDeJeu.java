0105726063c24f032dcc160c5a4e59b4bf3822c3
Aryamaan Kunwar <aryamaan3@gmail.com>
2020-10-08 15:18:21 +0200

    erreur

--- src/main/java/moteurdejeu/MoteurDeJeu.java
+++ src/main/java/moteurdejeu/MoteurDeJeu.java
@@ -10,13 +10,13 @@ public class MoteurDeJeu{
 
     public static void main(String[] args) {
         int nbjoueurs = 1; //pour l'instant seulement 1 joueur
-        System.out.println("Il y a %d joueur(s)", nbjoueurs);
-        System.out.println("\n Debut du jeu...");
+        System.out.println("Il y a %d joueur(s)"+nbjoueurs+"");
+        System.out.println("Debut du jeu...");
 
         Joueurs j1 = new Joueurs(1);
         CarteOuvrier c1 = new CarteOuvrier("ouvrier");
 
-        System.out.println("\nVoici vos choix" + c1 +"");
+        System.out.println("Voici vos choix" + c1 +"");
 
         private void piocher (j1, c1){
 
