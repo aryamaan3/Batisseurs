552f267adf55b1f8c3a20a8d524bb2a04ac85443
Aryamaan Kunwar <aryamaan3@gmail.com>
2020-10-08 15:45:14 +0200

    correction

--- src/main/java/moteurdejeu/MoteurDeJeu.java
+++ src/main/java/moteurdejeu/MoteurDeJeu.java
@@ -2,22 +2,21 @@ package moteurdejeu;
 
 
 import cartes.Cartes;
-import cartes.Cartes;
 import joueurs.Joueurs;
 
 
 
-public class MoteurDeJeu{
+public class MoteurDeJeu {
 
     public static void main(String[] args) {
         int nbjoueurs = 1; //pour l'instant seulement 1 joueur
-        System.out.println("Il y a %d joueur(s)"+nbjoueurs+"");
+        System.out.println("Il y a %d joueur(s)" + nbjoueurs + "");
         System.out.println("Debut du jeu...");
 
-        Joueurs j1 = new Joueurs(1);
+        //Joueurs j1 = new Joueurs(1);
         Cartes c1 = new Cartes("ouvrier");
 
-        System.out.println("Voici vos choix" + c1 +"");
+        System.out.println("Voici vos choix" + c1 + "");
 
         piocher(c1);
 
@@ -26,7 +25,8 @@ public class MoteurDeJeu{
 
     static Cartes DeckJoueur1[];  //Contiendra des objects "cartes" que possède le joueur 1
 
-    private static void piocher(Cartes Carte){
+    private static void piocher(Cartes Carte) {
         DeckJoueur1[0] = Carte;
-    }}
+    }
+}
 
