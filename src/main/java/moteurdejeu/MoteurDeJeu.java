562a01d9b554dfe8e2d0e5a32b279a82a3a777df
Aryamaan Kunwar <aryamaan3@gmail.com>
2020-10-08 14:46:31 +0200

    modification moteur

--- src/main/java/moteurdejeu/MoteurDeJeu.java
+++ src/main/java/moteurdejeu/MoteurDeJeu.java
@@ -9,8 +9,8 @@ import joueurs.Joueurs;
 public class MoteurDeJeu{
 
     public static void main(String[] args) {
-        Joueurs joueur1 = new Joueurs();
-        CarteOuvrier carte = new CarteOuvrier("Compagnon",4,4,3,0,0);
+        //Joueurs joueur1 = new Joueurs();
+        //CarteOuvrier carte = new CarteOuvrier("Compagnon",4,4,3,0,0);
 
 
 
