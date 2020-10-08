643b97178986fa8edb0ba3b02454e70c84b6fe91
Axel <axel.maissa@etu.unice.fr>
2020-10-08 16:19:53 +0200

    'Modification de Cartes.java'

--- src/main/java/cartes/Cartes.java
+++ src/main/java/cartes/Cartes.java
@@ -16,6 +16,6 @@ public class Cartes {
         return "Cartes{ " +
                 "id=" + idCart +
                 " type='" + type +'\'' +
-                ' }';
+                " }";
     }
 }
