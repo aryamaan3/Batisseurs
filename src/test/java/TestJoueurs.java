36a339f6822af1e407663ad671b396b88391c475
Axel <axel.maissa@etu.unice.fr>
2020-10-08 17:35:42 +0200

    TestJoueurs

--- src/test/java/TestJoueurs.java
+++ src/test/java/TestJoueurs.java
@@ -1,9 +1,18 @@
 import cartes.Cartes;
-
+import joueurs.Joueurs;
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.Assertions.*;
 
 public class TestJoueurs {
+    /*
     @Test
-    public Cartes
+    public void testJoueur() {
+        TestSignature test = new TestSignature(Joueurs.class);
+
+        test.publicConstructors(1);
+        test.publicConstructor(int.class);
+    }
+    */
+
+
 }
