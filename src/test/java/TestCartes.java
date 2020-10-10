06335bcc182e242b0d6f9825e70b0cb22296dc7c
Aryamaan Kunwar <aryamaan3@gmail.com>
2020-10-10 18:13:45 +0200

    Test Cartes

--- src/test/java/TestCartes.java
+++ src/test/java/TestCartes.java
@@ -1,2 +1,16 @@
-public class TestCartes {
+import cartes.Cartes;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TestCartes {
+    @Test
+    public void testcartes(){
+        int cond = 0;
+        Cartes test = new Cartes(1,"test" );
+        if (test.getIdCarte().equals("test")){
+            cond++;
+        }
+        assertEquals(1, cond);
+        //assertEquals("test", test);
+    }
 }
