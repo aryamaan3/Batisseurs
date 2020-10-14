2355ba8ea8bd73bcef0af5b9714813ae02c9d96c
Djade <djade.khaldi@gmail.com>
2020-10-14 12:01:30 +0200

    Modification du type tableau carteBat en mettant int, la valeur false devient 0 et le nom de la carte est enlevée, on pourra le récuperer avec l'id de la carte.
    Création d'un tableau avec les noms des cartes.

--- src/main/java/cartes/cartesbatiments/CarteBatiments.java
+++ src/main/java/cartes/cartesbatiments/CarteBatiments.java
@@ -5,12 +5,14 @@ import cartes.Cartes;
 public class CarteBatiments extends Cartes {
 
     int gainEcu,gainPoints;
-    public static Object[][] carteBat = {{0,"la cabane",0,1,1,0,8,0,0,0,0,0,false,-1},
-            {1,"la tonnelle",1,0,0,1,8,0,0,0,0,0,false,-1}, {2,"la cabane perchee",0,2,1,0,6,1,0,0,0,0,false,-1},
-            {3,"la hutte de paille",1,0,0,2,6,1,0,0,0,0,false,-1}, {4,"le lavoir",0,1,0,2,8,0,0,0,0,0,false,-1},
-            {5,"le pont en pierre",2,0,1,0,6,1,0,0,0,0,false,-1}, {6,"le pont couvert",0,1,2,0,6,1,0,0,0,0,false,-1},
-            {7,"la maison urbaine",2,0,2,1,10,2,0,0,0,0,false,-1}, {8,"la maisonnette",2,1,0,2,10,2,0,0,0,0,false,-1}};
+    public static int[][] carteBat = {{0,0,1,1,0,8,0,0,0,0,0,0,-1},
+            {1,1,0,0,1,8,0,0,0,0,0,0,-1}, {2,0,2,1,0,6,1,0,0,0,0,0,-1},
+            {3,1,0,0,2,6,1,0,0,0,0,0,-1}, {4,0,1,0,2,8,0,0,0,0,0,0,-1},
+            {5,2,0,1,0,6,1,0,0,0,0,0,-1}, {6,0,1,2,0,6,1,0,0,0,0,0,-1},
+            {7,2,0,2,1,10,2,0,0,0,0,0,-1}, {8,2,1,0,2,10,2,0,0,0,0,0,-1}};
 
+    public static String[] cartBatName = {"la cabane", "la tonnelle", "la cabane perchee", "la hutte de paille", "le lavoir", "le pont en pierre",
+        "le pont couvert", "la maison urbaine", "la maisonnette"};
 
     public CarteBatiments(int id, String nom, int gainEcu, int  gainPoints, int bois, int tuile, int savoir, int pierre, boolean construit, int idjoueur ) {
         super(id, nom, bois, tuile, savoir, pierre);
