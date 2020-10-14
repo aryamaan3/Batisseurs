fbf8ab566fe636b1df584a141cb4904edb62dbfc
sebastienchoisy <choisysebastien@gmail.com>
2020-10-14 16:49:01 +0200

    modification Deck.java et TestDeck.java, pour générer un deck plus facilement

--- src/main/java/deck/Deck.java
+++ src/main/java/deck/Deck.java
@@ -5,18 +5,14 @@ import cartes.cartesbatiments.CarteBatiments;
 import cartes.cartesouvrier.CarteOuvriers;
 public class Deck {
     String type;
+    Cartes[] deck;
 
-     public Deck (String type){
+
+     public Deck (String type) throws Exception {
          this.type = type;
 
-     }
-     /* On crée la méthode CreateDeck pour créer un deck ouvrier ou un deck batiment au début de la partie, on récupère
-     les cartes grâce aux tableaux dans la classe CarteBatiments et la classe CarteOuvrier */
-     public Cartes[] CreateDeck() throws Exception {
-
-        // On crée le deck si le type du deck est "ouvrier"
          if(type.equals("ouvrier")){
-             Cartes []deck = new Cartes[CarteOuvriers.carteOuv.length];
+             deck = new CarteOuvriers[CarteOuvriers.carteOuv.length];
              for(int i=0;i<CarteOuvriers.carteOuv.length;i++){
                  String nom;
                  if(CarteOuvriers.carteOuv[i][1] == 1){
@@ -25,33 +21,37 @@ public class Deck {
                      nom = "compagnon";
                  }
                  deck[i] = new CarteOuvriers(CarteOuvriers.carteOuv[i][0],nom,
-                                             CarteOuvriers.carteOuv[i][2],
-                                             CarteOuvriers.carteOuv[i][3],
-                                             CarteOuvriers.carteOuv[i][4],
-                                             CarteOuvriers.carteOuv[i][5],
-                                             CarteOuvriers.carteOuv[i][6],
-                                             CarteOuvriers.carteOuv[i][7],
-                                             CarteOuvriers.carteOuv[i][8]);
+                         CarteOuvriers.carteOuv[i][2],
+                         CarteOuvriers.carteOuv[i][3],
+                         CarteOuvriers.carteOuv[i][4],
+                         CarteOuvriers.carteOuv[i][5],
+                         CarteOuvriers.carteOuv[i][6],
+                         CarteOuvriers.carteOuv[i][7],
+                         CarteOuvriers.carteOuv[i][8]);
              }
-             return deck;
          } // On crée le deck si le type du deck est "batiment"
          else if(type.equals("batiment")){
-             Cartes []deck = new Cartes[CarteBatiments.carteBat.length];
+             deck = new CarteBatiments[CarteBatiments.carteBat.length];
              for(int i=0;i<CarteBatiments.carteBat.length;i++){
                  deck[i] = new CarteBatiments(i,CarteBatiments.carteBatName[i],
-                                                CarteBatiments.carteBat[i][1],
-                                                CarteBatiments.carteBat[i][2],
-                                                CarteBatiments.carteBat[i][3],
-                                                CarteBatiments.carteBat[i][4],
-                                                CarteBatiments.carteBat[i][5],
-                                                CarteBatiments.carteBat[i][6],
-                                                CarteBatiments.carteBat[i][7],
-                                                CarteBatiments.carteBat[i][8]);
+                         CarteBatiments.carteBat[i][1],
+                         CarteBatiments.carteBat[i][2],
+                         CarteBatiments.carteBat[i][3],
+                         CarteBatiments.carteBat[i][4],
+                         CarteBatiments.carteBat[i][5],
+                         CarteBatiments.carteBat[i][6],
+                         CarteBatiments.carteBat[i][7],
+                         CarteBatiments.carteBat[i][8]);
              }
-             return deck;
-
          } else {
              throw new Exception("deck doit être ouvrier ou batiment");
          }
+
      }
+
+    public Cartes[] getDeck() {
+        return deck;
+    }
+
+
 }
--- src/test/java/deck/TestDeck.java
+++ src/test/java/deck/TestDeck.java
@@ -8,17 +8,15 @@ public class TestDeck {
     // On fait un premier test pour vérifier qu'on récupère bien les cartes dans le deck des batiments
     @Test
     public void testDeckBatiments() throws Exception {
-        Deck deck1 = new Deck("batiment");
-        Cartes [] deckBatiments = deck1.CreateDeck();
-        Cartes carteTest = deckBatiments[6];
+        Cartes[] deck = new Deck("batiment").getDeck();
+        Cartes carteTest = deck[6];
         System.out.print(carteTest);
     }
     // On fait un second test pour vérifier qu'on récupère bien les cartes dans le deck des batiments
-    @Test
+  @Test
     public void testDeckOuvriers() throws Exception {
-        Deck deck1 = new Deck("ouvrier");
-        Cartes [] deckOuvrier = deck1.CreateDeck();
-        Cartes carteTest = deckOuvrier[10];
+        Cartes[] deck = new Deck("ouvrier").getDeck();
+        Cartes carteTest = deck[10];
         System.out.print(carteTest);
     }
 
