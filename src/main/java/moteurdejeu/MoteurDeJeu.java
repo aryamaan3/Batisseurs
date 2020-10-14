a0915ddc0ae297f463201e001446d0f7b209a8e6
Axel <axel.maissa@etu.unice.fr>
2020-10-14 09:11:20 +0200

    Ajout des commentaires de la class MoteurDeJeu.java + implementation d'une nouvelle variable ZoneDeConstruction + méthode choisirChantier

--- src/main/java/moteurdejeu/MoteurDeJeu.java
+++ src/main/java/moteurdejeu/MoteurDeJeu.java
@@ -8,10 +8,26 @@ import joueurs.Joueurs;
 
 public class MoteurDeJeu {
 
-    public static Cartes[][] DeckJoueur = new Cartes[2][2];  //Contiendra des objects "cartes" que possède les joueur
+    // Desk de carte : la première carte du joueur 0 sera à l'indice [0][0]
+    // Contient donc les cartes ouvrier de TOUS les joueurs
+    public static Cartes[][] DeckOuvrier = new Cartes[2][2];  //Contiendra des objects "cartes" que possède les joueur
 
-    public static void piocher(Joueurs joueur, Cartes carte) {
-        DeckJoueur[joueur.getId()][0] = carte; // Regroupe les decks de tout le monde, le deck du la première carte du joueur 1 sera à la position Deck[0][0]
+    // Variable qui contient les zones de construction (où les batiments sont placés)
+    // Contient les cartes en construction de TOUS les joueurs
+    // (ex : ZoneDeConstruction[1][2] joueur 1, carte numéro 3 (indice 2)
+    public static Cartes[][] ZoneDeConstruction = new Carte[2][2];
+
+    // Methode permettant à un joueur de piocher une carte (les deux sont en paramètre de la méthode
+    // Ajoute la carte piocher à l'indice 0 du deck du joueur
+    public static void piocher(Joueurs joueur, Cartes carteOuvrier) {
+        DeckOuvrier[joueur.getId()][0] = carteOuvrier; // Regroupe les decks de tout le monde, le deck du la première carte du joueur 1 sera à la position Deck[0][0]
+    }
+
+    // Permet à un joueur de placer une carteOuvrier sur une carteBatiment
+    // OU de placer une carte batiment sur le plateau
+    // Permet à la fois de placer une carte batiment dans sa Zone
+    public void choisirChantier(Joueurs joueur, Cartes carteBatiment, Cartes carteOuvrier){
+
     }
 
     public static void main(String[] args) {
@@ -30,8 +46,10 @@ public class MoteurDeJeu {
 
         piocher(j1,c1);
 
-        System.out.println("Le joueur "+j1.getId()+ " possède ces cartes : "+DeckJoueur[j1.getId()][0]);
-            if(DeckJoueur[j1.getId()][0].getIdCarte().equals("ouvrier")){
+        System.out.println("Le joueur "+j1.getId()+ " possède ces cartes : "+DeckOuvrier[j1.getId()][0]);
+
+        // Condition de victoire
+            if(DeckOuvrier[j1.getId()][0].getIdCarte().equals("ouvrier")){
                 System.out.println("Vous avez gagné");
                 break;
             }
