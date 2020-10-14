b30befe178aa6d209a21fe861ac6a57e3c63ec18
Djade <djade.khaldi@gmail.com>
2020-10-14 10:42:41 +0200

    Ajout du tableau contenant les cartes batiments.

--- src/main/java/cartes/cartesbatiments/CarteBatiments.java
+++ src/main/java/cartes/cartesbatiments/CarteBatiments.java
@@ -5,6 +5,11 @@ import cartes.Cartes;
 public class CarteBatiments extends Cartes {
 
     int gainEcu,gainPoints;
+    String[][] carteBat = {{"0","la cabane","0","1","1","0","8","0","0","0","0","0","FALSE","-1"},
+            {"1","la tonnelle","1","0","0","1","8","0","0","0","0","0","FALSE","-1"}, {"2","la cabane perchee","0","2","1","0","6","1","0","0","0","0","FALSE","-1"},
+            {"3","la hutte de paille","1","0","0","2","6","1","0","0","0","0","FALSE","-1"}, {"4","le lavoir","0","1","0","2","8","0","0","0","0","0","FALSE","-1"},
+            {"5","le pont en pierre","2","0","1","0","6","1","0","0","0","0","FALSE","-1"}, {"6","le pont couvert","0","1","2","0","6","1","0","0","0","0","FALSE","-1"},
+            {"7","la maison urbaine","2","0","2","1","10","2","0","0","0","0","FALSE","-1"}, {"8","la maisonnette","2","1","0","2","10","2","0","0","0","0","FALSE","-1"}};
 
 
     public CarteBatiments(int id, String nom, int gainEcu, int  gainPoints, int bois, int tuile, int savoir, int pierre) {
