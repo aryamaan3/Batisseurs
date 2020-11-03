6e061e5614be22de48ff0941da86233c3d0c6bf4
Burel <paul.burel@etu.unice.fr>
2020-11-03 11:13:20 +0100

    Création des tests de MoteurDeJeux #62

--- src/main/java/decks/Shuffle.java
+++ src/main/java/decks/Shuffle.java
@@ -1,21 +1,20 @@
 package decks;
 
-
 import cartes.cartesouvrier.CarteOuvriers;
 import java.util.Random;
 
 public class Shuffle {
 
-    /*public void shuffleBat(DeckBatiments d){
+    public void shuffleBat(DeckBatiments[] d){
         Random gen = new Random();
-        for (int i = d.len() - 1; i > 0; i--){
+        for (int i = d.length - 1; i > 0; i--){
             int indice = gen.nextInt(i + 1);
             // je swap
-            CarteBatiments a = d.get(indice);
-            d.get(indice) = d.get(i);
-            d.get(i) = a;
+            DeckBatiments a = d[indice];
+            d[indice] = d[i];
+            d[i] = a;
         }
-    }*/
+    }
 
     public static void shuffleOuvrier(CarteOuvriers[] d){
         Random gen = new Random();
