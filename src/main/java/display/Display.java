f616dfa1dde5729a2c4e51bb928d0b97dfacec0f
Axel <axel.maissa@etu.unice.fr>
2020-11-04 11:53:28 +0100

    Story #48 Task #69
    Ajout d'une méthode displayEtatChantiersDuJoueur()

--- src/main/java/display/Display.java
+++ src/main/java/display/Display.java
@@ -10,23 +10,35 @@ public class Display {
     public static void displayOuvriersDuJoueur(int idJoueur){
         // On itère sur le DeckOuvrier du moteur de jeu
         // si on trouve un ouvrier qui à un assign == idJoueur donné en paramètre : on l'imprime
+        System.out.println("Le joueur " + idJoueur + " possède ce(s) ouvrier(s) :");
         for(int i = 0; i < MoteurDeJeu.DeckOuvrier.size(); i ++){
             if(idJoueur == MoteurDeJeu.DeckOuvrier.get(i).getIdjoueur()){
-                System.out.println("Le joueur " + idJoueur
-                        + " possède " + MoteurDeJeu.DeckOuvrier.get(i).getName()
+                System.out.println(" - " + MoteurDeJeu.DeckOuvrier.get(i).getName()
                         + " (id = " + MoteurDeJeu.DeckOuvrier.get(i).getId() + ")");
             }
         }
     }
 
+    /**
+     *  Classe qui imprime les chantiers d'un joueur
+     * @param idJoueur id joueur
+     */
     public static void displayChantierDuJoueur(int idJoueur){
         // On itère sur le DeckBatiment du moteur de jeu
         // si on trouve un batiment qui à un assign == idJoueur donné en paramètre : on l'imprime
+        System.out.println("Le joueur " + idJoueur + " construit ce(s) chantier(s) :");
         for(int i = 0; i < MoteurDeJeu.DeckBatiment.size(); i ++){
             if(idJoueur == MoteurDeJeu.DeckBatiment.get(i).getIdjoueur()){
-                System.out.println("Le joueur " + idJoueur
-                        + " construit " + MoteurDeJeu.DeckBatiment.get(i).getName()
+                System.out.println(" - " + MoteurDeJeu.DeckBatiment.get(i).getName()
                         + " (id = " + MoteurDeJeu.DeckBatiment.get(i).getId() + ")");
+            }
+        }
+    }
+
+    public static void displayEtatChantiersDuJoueur(int idJoueur){
+        for(int i = 0; i < MoteurDeJeu.DeckBatiment.size(); i ++){
+            if(idJoueur == MoteurDeJeu.DeckBatiment.get(i).getIdjoueur()){
+                System.out.println(MoteurDeJeu.DeckBatiment.get(i).toString());
             }
         }
     }
