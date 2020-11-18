76093dd073fe6dd25367a9010befc99d794389fe
Aryamaan Kunwar <aryamaan3@gmail.com>
2020-11-18 11:13:11 +0100

    #91 ajout de méthodes qui verifient si joueur possede assez d'écus pour passer un tour dans IA

--- src/main/java/moteurdejeu/MoteurDeJeu.java
+++ src/main/java/moteurdejeu/MoteurDeJeu.java
@@ -106,13 +106,13 @@ public class MoteurDeJeu{ //Controle le deroulement du jeu
         System.out.println("Il y a "+nombreDeJoueurActifs+" joueur(s)");
         System.out.println("Debut du jeu...");
 
-        // On créer un objet IA
-        IA IAduJoueur1 = new IA(j1, DeckOuvrier, DeckBatiment);
-        IA IAduJoueur2 = new IA(j2, DeckOuvrier, DeckBatiment);
 
         // On crée les objets compteurs pour chaque joueur
         Compteur c1 = new Compteur(j1.getId());
         Compteur c2 = new Compteur(j2.getId());
+        // On créer un objet IA
+        IA IAduJoueur1 = new IA(j1, DeckOuvrier, DeckBatiment, c1);
+        IA IAduJoueur2 = new IA(j2, DeckOuvrier, DeckBatiment, c2);
 
         whileTour:
         while (true){ //loop pour chaque tour
@@ -123,7 +123,7 @@ public class MoteurDeJeu{ //Controle le deroulement du jeu
                 if (i == 0) { //actions du joueur 1
                     System.out.println("------------------ Joueur n°" + (i + 1) + "------------------");
                     displayActions(c1);
-                    IAduJoueur1.ActionsIA(c1,bourse.get(0));
+                    IAduJoueur1.ActionsIA();
                     displayActions(c1);
                     /* A supprimer si display marche bien
                     System.out.println("Le joueur " + (i +1) + " a selectionné un ouvrier " + CarteOuvriersSurTable.get(3).getName() + " et un chantier " + CarteBatimentsSurTable.get(1).getName());
@@ -153,7 +153,7 @@ public class MoteurDeJeu{ //Controle le deroulement du jeu
                 if (i == 1) { //actions du joueur 2
                     System.out.println("------------------ Joueur n°" + (i +1) + "------------------");
                     displayActions(c2);
-                    IAduJoueur2.ActionsIA(c2, bourse.get(1));
+                    IAduJoueur2.ActionsIA();
                     displayActions(c2);
                     /*System.out.println("Le joueur " + (i + 1) + " a selectionné un ouvrier " + CarteOuvriersSurTable.get(4).getName() + " et un chantier " + CarteBatimentsSurTable.get(4).getName());
                     System.out.println("Sur le chantier " + CarteBatimentsSurTable.get(4).getName() + " on a l'ouvrier " + CarteBatimentsSurTable.get(4).getIdOuvrier().get(0));
