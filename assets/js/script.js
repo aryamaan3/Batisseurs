ef4f7923a245cabbd85fa85813c09e8d50e43b9f
Aryamaan Kunwar <aryamaan3@gmail.com>
2021-01-14 18:27:17 +0100

    modif site

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,13 @@
 function unePartie(){
     document.getElementsByTagName("iframe")[0].setAttribute("src", "./assets/unePartie.txt");
+    changeColor();
 }
 function multPartie(){
     //populatePre('./assets/stats500.txt');
     document.getElementsByTagName("iframe")[0].setAttribute("src", "./assets/stats500.txt");
+    changeColor();
+}
+function changeColor(){
+    let doc = document.getElementsByTagName("iframe");
+    doc[0].style.backgroundColor = "#FFFFFF";
 }
